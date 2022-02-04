979b097118a5d19c463e9b131eb945b7dcb9b2c1
Andrei Fadeev <fadeev.95@icloud.com>
2022-02-04 20:00:26 +0200

    Initial Commit

---
+++ FirstGit2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstGit2022
+//
+//  Created by Andrei Fadeev on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
